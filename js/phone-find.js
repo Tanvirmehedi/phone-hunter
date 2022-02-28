2447167a904d31bb82dfed2de35bb5bcd4eeac3c
Tanvirmehedi <tjmehedi201@gmail.com>
2022-03-01 03:15:57 +0600

    Search By Phone Added

--- js/phone-find.js
+++ js/phone-find.js
@@ -1,22 +1,70 @@
 // Get Phone Api By search
 
 const getPhoneApi = async () => {
-  const baseUrl = `https://openapi.programming-hero.com/api/phones?search=samsung`;
+  const inputBox = document.getElementById("search-input");
+  const inputValue = inputBox.value;
+  if (inputValue === "") {
+    errMessage("Give A valid Phone Name!");
+    inputBox.value = "";
+  } else {
+    const baseUrl = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    try {
+      const request = await fetch(baseUrl);
+      const phoneData = await request.json();
+      if (phoneData.data.length === 0) {
+        errMessage(`No Data Found in ${inputValue} Please Try Another `);
+      } else {
+        displaySearchResult(phoneData.data);
+      }
+      inputBox.value = "";
+    } catch (err) {
+      errMessage(err);
+    }
+  }
 };
 
-// Get The Input Value
-const inputField = () => {
-  const inputBox = document.getElementById("search-input");
+const displaySearchResult = (result) => {
+  const phoneGridBox = document.getElementById("phone-grid-box");
+  phoneGridBox.innerText = "";
+  const firstTwentyItems = result.slice(0, 20);
+  for (const item of firstTwentyItems) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="flex justify-around items-center border-2 rounded-md mx-auto w-11/12">
+            <div class="images py-3">
+                <img src="${item.image}" alt="" />
+            </div>
+
+            <div class="info-box self-start py-3 px-2">
+                <h2>
+                <span class="block">Product Name: </span>${item.phone_name}</h2>
+                <h2>
+                <span class="inline-block">Brand Name: </span> ${item.brand}</h2>
+
+                <button class="bg-slate-700 text-slate-100 font-bold px-3 py-1 rounded drop-shadow-md font__lobster" onclick="getDetailsById('${item.slug}')" >
+                Details
+                </button>
+            </div>
+            
+        </div>
+    `;
+    phoneGridBox.appendChild(div);
+  }
+};
+
+// Get Phone Details
+const getDetailsById = async (slug) => {
+  const baseUrl = `https://openapi.programming-hero.com/api/phone/${slug}`;
   try {
-    if (inputBox.value === "") {
-      errMessage("Pleas Input your Phone Name !");
-    } else {
-      console.log(inputBox.value);
-    }
+    const request = await fetch(baseUrl);
+    const data = await request.json();
+    console.log(data.data);
   } catch (err) {
     errMessage(err);
   }
 };
+
+getPhoneApi();
 
 // Error Message
 const errMessage = (text) => {
